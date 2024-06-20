9f7afb97906ca8832b609a0e17a26913d8e5e108
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-06-20 16:10:22 +1000

    Full simulation of gas colliding

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import imageio.v2 as imageio
+import os
 
 import matplotlib.animation as animation
 
 class particle:
 	'''A particle is a circle with position, velocity and radius.'''
-	def __init__(self, x, y, dx, dy, r=0.01, c = 'black', f = False):
+	def __init__(self, x, y, dx, dy, r=0.01, m=1,c = 'black', f = False):
 		self.x = x
 		self.y = y
 		self.dx = dx
@@ -14,6 +16,7 @@ class particle:
 		self.color = c
 		self.radius = r
 		self.fill = f
+		self.mass = m
 
 	def draw(self, ax):
 		'''Draw onto graph'''
@@ -39,21 +42,31 @@ class particle:
 			self.y = 1-self.radius
 			self.dy = -self.dy
 
-class animation:
+	def bounce(self, molecule):
+		'''Elastic collision between two molecules'''
+		dist = np.linalg.norm(np.subtract((self.x,self.y),(molecule.x,molecule.y)))
+		vself = (self.dx,self.dy)
+		vother = (molecule.dx,molecule.dy)
+		posself = (self.x,self.y)
+		posother = (molecule.x,molecule.y)
+		(self.dx,self.dy) = np.subtract(vself, ((2*molecule.mass)/(self.mass+molecule.mass))*(np.dot(np.subtract(vself,vother),np.subtract(posself,posother))/(dist**2))*np.subtract(posself,posother))
+
+class frame:
 	'''The animation'''
 	def __init__(self, n):
 		self.number = n
+		self.molecules = np.ndarray((n,),dtype=object)
 	
 	def drawParticles(self, ax):
-		molecules = np.ndarray((self.number,),dtype=object)
-		dx = 0
-		dy = 0
 		for i in range(self.number):
 			x = random.random()*(0.98)+0.01
 			y = random.random()*(0.98)+0.01
-			molecules[i]=particle(x,y,dx,dy)
-		for molecule in molecules:
-			for other in molecules:
+			dx = (random.random()-0.5)*0.02
+			dy = (random.random()-0.5)*0.02
+			self.molecules[i]=particle(x,y,dx,dy)
+		for molecule in self.molecules:
+			molecule.boundary()
+			for other in self.molecules:
 				if molecule.collision(other) and molecule != other:
 					del molecule
 					break
@@ -62,8 +75,33 @@ class animation:
 			except UnboundLocalError:
 				pass
 
+	def update(self, ax):
+		for molecule in self.molecules:
+			molecule.boundary()
+			for other in self.molecules:
+				if molecule.collision(other) and molecule != other:
+					molecule.bounce(other)
+			molecule.x += molecule.dx
+			molecule.y += molecule.dy
+			particle.draw(molecule, ax)
 
+def create_gif(path, frames):
+	images = []
+	for i in range(frames):
+		filename = os.path.join(path,f"{i}.png")
+		images.append(imageio.imread(filename))
+	imageio.mimsave("animation.gif", images, duration=frames/5)
+
+runs = 50
 fig, ax = plt.subplots()
-ani = animation(100)
-ani.drawParticles(ax)
-fig.savefig('animation.png')
+anime = frame(100)
+anime.drawParticles(ax)
+fig.savefig('0.png')
+for t in range(runs-1):
+	plt.clf()
+	plt.close()
+	fig, ax = plt.subplots()
+	anime.update(ax)
+	fig.savefig(str(t+1)+'.png')
+	plt.close()
+create_gif(os.path.dirname(__file__), runs)
