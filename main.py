29bc25d58863ce74d5f0bf63f759450817aedc27
AriaZhao <50545494+katyushazhao@users.noreply.github.com>
2024-06-20 12:04:18 +1000

    Draw circle on graph

--- main.py
+++ main.py
@@ -6,5 +6,23 @@ import matplotlib.animation as animation
 class particle:
 	'''A particle is a circle with position, velocity and radius.'''
 	def __init__(self, x, y, dx, dy, r=0.01, c = 'black', f = False):
-		circle = plt.Circle((x,y),r, color = c, fill = f)
+		self.x = x
+		self.y = y
+		self.color = c
+		self.radius = r
+		self.fill = f
+
+	def draw(self, ax):
+		'''Draw onto graph'''
+		circle = plt.Circle((self.x,self.y),self.radius, color = self.color, fill = self.fill)
 		ax.add_patch(circle)
+
+class animation:
+	'''The animation'''
+	def __init__(self):
+		fig, ax = plt.subplots()
+		molecule = particle(0.7,0.3,0,0)
+		particle.draw(molecule, ax)
+		fig.savefig('animation.png')
+
+ani = animation()
--- test.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-circle1 = plt.Circle((0.5,0.5),0.01, color = 'black', fill=False)
-fig, ax = plt.subplots()
-ax.add_patch(circle1)
-fig.savefig('plotcircles.png')
